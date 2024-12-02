0f6dbef5c8a8d2a4537cfb8d404b5d2af1bf4e06
M.A. Asardeen <asardeenbse@gmail.com>
2024-12-02 13:59:08 +0530

    route setup before

---
+++ react-route/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        {/* <Home />
+        <About />
+        <Contact /> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
